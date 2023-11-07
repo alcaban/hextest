b79b9971af9e91006cfc6bd62d1bc63922fb339e
alcaban <143461668+alcaban@users.noreply.github.com>
2023-11-07 14:24:57 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Initialize the map
+var map = L.map('map').setView([0, 0], 3);
+
+// Add a basemap layer (e.g., OpenStreetMap)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+}).addTo(map);
+
+// Create a hexbin layer
+var hexLayer = L.hexbinLayer({
+    radius: 12, // Adjust the hexbin size
+    opacity: 0.7, // Adjust the opacity
+    colorScaleExtent: [1, 100], // Adjust color scale extent
+}).addTo(map);
+
+// Load GeoJSON data
+fetch('your_geojson_data.geojson')
+    .then((response) => response.json())
+    .then((data) => {
+        hexLayer.data(data.features); // Pass the GeoJSON features to the hexbin layer
+        map.fitBounds(hexLayer.dataBounds); // Fit the map to the data bounds
+    })
+    .catch((error) => console.error('Error loading GeoJSON:', error));
